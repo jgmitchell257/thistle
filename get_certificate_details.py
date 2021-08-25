cca3d1b6c96d7dba0d109ab446e8f092cbdb072e
Justin G. Mitchell <jgmitchell@gmail.com>
2021-08-25 16:16:17 -0600

    Clean up unused code

--- get_certificate_details.py
+++ get_certificate_details.py
@@ -1,22 +1,7 @@
 import json
 import socket
 import ssl
-"""import OpenSSL
 
-
-
-
-cert_data = ssl.get_server_certificate((hostname, "443"))
-cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_data)
-subject = cert.get_subject()
-issued_to = subject.CN
-issuer = cert.get_issuer()
-issued_by = issuer.CN
-expires = cert.get_notAfter()
-
-print(f"Issued to: {issued_to}")
-print(f"Issued by: {issued_by}")
-print(f"Expires: {expires}")"""
 
 def get_certificate(host):
     context = ssl.create_default_context()
