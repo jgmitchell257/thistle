e5d047fc10dedf1e967a63e0b60c9a281d256649
Justin G. Mitchell <jgmitchell@gmail.com>
2021-08-25 15:13:50 -0600

    Fix issue with NXDOMAIN

--- thistle/dns.py
+++ thistle/dns.py
@@ -4,7 +4,7 @@ import subprocess
 def check_collab_edge(domain: str):
     edge = "_collab-edge._tls." + domain
     nslookup = subprocess.run(["nslookup", "-type=SRV", edge], encoding="UTF-8", capture_output=True)
-    if "SERVFAIL" in nslookup.stdout:
+    if "NXDOMAIN" in nslookup.stdout:
         return False
-    elif edge in nslookup.stdout:
+    else:
         return True
