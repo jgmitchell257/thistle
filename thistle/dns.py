fee1025234db106c2b6c0634b15fe309f912a134
Justin G. Mitchell <jgmitchell@gmail.com>
2021-08-30 15:13:31 -0600

    fixing things

--- thistle/dns.py
+++ thistle/dns.py
@@ -1,4 +1,5 @@
 import subprocess
+import ipaddress
 
 
 def check_collab_edge(domain: str):
@@ -8,3 +9,26 @@ def check_collab_edge(domain: str):
         return False
     else:
         return True
+
+
+def ip_from_ping(hostname):
+    grab_output = subprocess.run(["ping","-c","4",hostname],capture_output=True)
+    bits = grab_output.stdout.decode("utf-8")
+    bucket = bits.split()
+    ip = bucket[2].strip("[,]")
+    if ip == "could":
+        ip = "Not found"
+    return ip
+
+
+def ip_from_nslookup(hostname):
+    nslookup_raw = subprocess.run(["nslookup",hostname],capture_output=True)
+    nslookup_bits = nslookup_raw.stdout.decode("utf-8")
+    nslookup_bucket = nslookup_bits.split()   
+    try:
+        ip = nslookup_bucket[7]
+        ipaddress.IPv4Address(ip)
+        return ip
+    except:
+        ip_not_found = "Not found"
+        return ip_not_found
--- thistle/file_handler.py
+++ thistle/file_handler.py
@@ -1,4 +1,9 @@
 def load_file_to_list(filename: str) -> list:
+    clean_data = []
     with open(filename, "r") as f:
-        file_data = f.readlines()
-        return file_data
+        raw_data = f.readlines()
+        for i in raw_data:
+                clean_file_input = i.strip("\n")
+                clean_data.append(clean_file_input)
+        f.close()
+    return clean_data
